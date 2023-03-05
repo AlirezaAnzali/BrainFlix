d47b09fb30eb0bf64d208bec34f49dc0fcc18cac
Ali <121503489+AlirezaAnzali@users.noreply.github.com>
2023-03-05 07:44:42 -0500

    add video structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import Video from './components/Video/Video';
+import { useState } from 'react';
+import videoDetails from "./data/video-details.json";
 
 function App() {
+const [currentVideo, setCurrentVideo]= useState(videoDetails[0]);
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Video currentVideo={currentVideo} />
     </div>
   );
 }
---
+++ src/components/Video/Video.js
@@ -0,0 +1,39 @@
+import "./Video.scss";
+import scrubbingIcon from "../../assets/images/scrub.svg"
+
+function Video({ currentVideo }) {
+  return (
+    <div className="video">
+      <video
+        className="video__video"
+        src={currentVideo.video}
+        poster={currentVideo.image}
+      ></video>
+      <div className="video__controls">
+        <button className="video__controls__play video__controls__button"></button>
+        <div className="video__controls__progress video__controls__button">
+          <div className="video__controls__progress__progress">
+            <div className="video__controls__progress__buffer"></div>
+            <div className="video__controls__progress__played"></div>
+            <div className="video__controls__progress__scrubber">
+              <img
+                className="video__controls__progress__scrubbing-icon"
+                src={scrubbingIcon}
+                alt="scrubbing icon"
+              />
+            </div>
+          </div>
+          <span className="video__controls__progress__time">
+            0:00 / {currentVideo.duration}
+          </span>
+        </div>
+        <div className="video__controls__button">
+          <button className="video__controls__fullscreen video__controls__button"></button>
+          <button className="video__controls__mute video__controls__button"></button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Video;
